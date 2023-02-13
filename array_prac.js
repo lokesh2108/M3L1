9d86543182afda3317809eeda4fe5624e0e9cde6
lokesh2108 <lokil4980@gmail.com>
2023-02-12 19:16:42 -0800

    done

--- array_prac.js
+++ array_prac.js
@@ -139,3 +139,103 @@ leastFavMovies[2]= "Mersal";
 movies = movies.concat(leastFavMovies);
 movies.sort().reverse();
 window.console.log(movies.slice(-1));
+
+
+// step 11
+var movies = [];
+movies[0]= "Vikram";
+movies[1]= "Leo";
+movies[2]= "Stranger Things";
+movies[3]= "Dark";
+movies[4]= "Zodiac";
+movies[5]="RRR";
+movies[6]="Pathaan";
+var leastFavMovies =[];
+leastFavMovies[0]= "Vivegam";
+leastFavMovies[1]= "Varisu";
+leastFavMovies[2]= "Mersal";
+movies = movies.concat(leastFavMovies);
+movies.sort().reverse();
+window.console.log(movies.shift());
+
+
+
+// step 12
+var movies = [];
+movies[0]= "Vikram";
+movies[1]= "Leo";
+movies[2]= "Stranger Things";
+movies[3]= "Dark";
+movies[4]= "Zodiac";
+movies[5]="RRR";
+movies[6]="Pathaan";
+var leastFavMovies =[];
+leastFavMovies[0]= "Vivegam";
+leastFavMovies[1]= "Varisu";
+leastFavMovies[2]= "Mersal";
+movies = movies.concat(leastFavMovies);
+movies.sort().reverse();
+var indexes=[]
+movies.forEach((movie,index) => {
+    if(leastFavMovies.includes(movie)){
+        indexes.push(index);
+    }
+});
+var likemovies =["96","Petta","KGF"]
+//window.console.log(indexes);
+indexes.forEach((index) => {
+    movies[index] = likemovies.shift();
+});
+
+//window.console.log(movies);
+
+// step 13
+var multimovies = [["Varisu", 1], ["KGF", 2], ["Dark", 3], ["leo", 4], ["Zodiac", 5]];
+
+var Mname = multimovies.filter(function(movie) {
+  return typeof movie[0] === "string";
+});
+
+window.console.log(Mname.map(movie => movie[0]));
+
+// step 14
+
+var employees = ["ZAK", "JESSICA", "MARK", "FRED", "SALLY"];
+
+var display = function(employees) {
+  window.console.log("Employees: ");
+  window.console.log("\n"); 
+  window.console.log("\n"); 
+  employees.forEach(function(i) {
+    window.console.log(i);
+  });
+};
+display(employees);
+
+
+// step 15
+
+var values = [58, '', 'abcd', true, null, false, 0];
+
+var filterVal = function(values){
+    return values.filter(function(i) {
+  return i !== false && i !== null && i !== 0  && i !== "";
+});
+};
+
+window.console.log(filterVal(values));
+
+// step 16
+var Random = function(i) {
+    return i[Math.floor(Math.random() * i.length)];
+  };
+var num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+window.console.log(Random(num));
+
+// step 17
+var LargestNumber = function(i){
+    return Math.max(...i);
+}
+
+var num = [12 , -2, 201 , 35 , -10 ,6 , 71 ];
+window.console.log(LargestNumber(num));
